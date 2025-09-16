420616f8b9e362ea1b78f1dde15ef87d739047c0
Atharva Choudhary <atharvavc04@gmail.com>
2025-09-16 22:32:57 +0100

    updated 'Experience' section'

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,13 +2,21 @@ import project1 from "../assets/projects/project-1.png";
 import project2 from "../assets/projects/project-2.png";
 import project3 from "../assets/projects/project-3.png";
 
-export const MAIN_CONTENT = `I am a first year Computer Science student based in London, United Kingdom.  I have a strong foundation in programming and a passion for technology, constantly exploring the fascinating world of digital security and its diverse applications.`;
+export const MAIN_CONTENT = `I am a placement year Computer Science student based in London. I am currently working as a Software Engineer at Elanco Animal Health.`;
 
 export const ABOUT_TEXT = "I’m Atharva, and I am currently pursuing a bachelor’s degree in computer science at Brunel University in the city of London. My academic journey is driven by a passion for digital security, and I’m continuously exploring the ever-evolving landscape of this critical field. Though I’m still exploring the various specializations within computer science, my enthusiasm keeps me engaged with the latest trends and breakthroughs in technology. Outside of my studies, I enjoy a well-rounded lifestyle. Video games offer me not just entertainment but also a sense of challenge and accomplishment. Additionally, my love for sports finds me eagerly following football and basketball, where the excitement of the games is always captivating.";
 
 export const EXPERIENCES = [
   {
-    year: "July 2024 - Present",
+    year: "June 2025 - Present",
+    role: "Software Engineer",
+    company: "Elanco Animal Health | Hook, UK",
+    description: `Currently working as a Software Engineer at Elanco Animal Health, contributing to the development and maintenance of software solutions that support the company's mission to improve animal health and well-being.\n Engaging in continuous learning and professional development to stay abreast of emerging technologies and best practices in software engineering.`,
+    technologies: ["Google Cloud", "Terraform", "Python", "Typescript"],
+  },
+
+  {
+    year: "July 2024 - October 2024",
     role: "Software Engineering Fellow",
     company: "Headstarter AI | New York, USA",
     description: `Built 5 AI apps and APIs using NextJS, OpenAI, Pinecone and StripeAPI.\n Coached by Amazon, Bloomberg and Capital One engineers on Aglie, CI/CD, Git and microservice patterns.\n Participated in 3 global hackathons, leading a team of 4 engineering fellows.`,
